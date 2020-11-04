b38ac3d6600e9b85c37d6206d599b072855b0cfc
Peladito <estebanlucianorey@gmail.com>
2020-11-04 19:27:13 -0300

    longitude and latitude to locations

--- interfaces/http/presenters/results.js
+++ interfaces/http/presenters/results.js
@@ -3,6 +3,10 @@ exports.present = function (data){
         data = data.toObject()
         data.id = data._id
         delete data._id
+        if(data.position){
+            let [longitude, latitude] = data.position.coordinates
+            Object.assign(data, {longitude, latitude})
+        }
     }
     return {status:200, data}
 }
