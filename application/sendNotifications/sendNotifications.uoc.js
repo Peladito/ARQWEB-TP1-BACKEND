6257dc33034214c1d66ba4374d6e4749ad1c6adf
Peladito <estebanlucianorey@gmail.com>
2020-11-15 16:00:56 -0300

    cron fix

--- application/sendNotifications/sendNotifications.uoc.js
+++ application/sendNotifications/sendNotifications.uoc.js
@@ -1,9 +1,9 @@
 const { SendNotificationsError } = require("./errors");
 
-exports.sendNotifications = ({fetchUnsentNotifications, sendNotifications}) => actor => async ({}) => {
+exports.sendNotifications = ({fetchUnsentNotifications, sendNotifications}) => async ({}) => {
     try{
         let notifications = await fetchUnsentNotifications({})
-        console.log(notifications)
+        console.log("Notifications to send:",notifications)
         await sendNotifications(notifications)
     }catch(error){
         throw new SendNotificationsError(error)
