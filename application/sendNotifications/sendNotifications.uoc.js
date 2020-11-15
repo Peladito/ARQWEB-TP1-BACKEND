c826a48f85d7351d855cb784b27a55d0b888acb8
Peladito <estebanlucianorey@gmail.com>
2020-11-15 15:54:54 -0300

    log

--- application/sendNotifications/sendNotifications.uoc.js
+++ application/sendNotifications/sendNotifications.uoc.js
@@ -3,6 +3,7 @@ const { SendNotificationsError } = require("./errors");
 exports.sendNotifications = ({fetchUnsentNotifications, sendNotifications}) => actor => async ({}) => {
     try{
         let notifications = await fetchUnsentNotifications({})
+        console.log(notifications)
         await sendNotifications(notifications)
     }catch(error){
         throw new SendNotificationsError(error)
